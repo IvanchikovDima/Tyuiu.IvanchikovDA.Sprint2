126cd4f8e6b058931c204a5b2d65171cd83e87d8
Tyuiu.IvanchikovDA@outlook.com <ivanchikov2022@internet.ru>
2023-10-20 21:44:49 +0500

    Task1V13

---
+++ Tyuiu.IvanchikovDA.Sprint2.Task1.V13/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IvanchikovDA.Sprint2.Task1.V13.Lib;
+
+namespace Tyuiu.IvanchikovDA.Sprint2.Task1.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int a = 145;
+            int b = 916;
+            int c = 164;
+            int d = 137;
+
+            bool[] res = new bool[6];
+            res ds.GetLogicOperations(a, b, c, d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
