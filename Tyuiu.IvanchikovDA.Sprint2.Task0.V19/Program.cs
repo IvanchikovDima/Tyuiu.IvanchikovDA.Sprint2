dbb108a6424fcb96fc33785d91c29d6cd4fd49bf
Tyuiu.IvanchikovDA@outlook.com <ivanchikov2022@internet.ru>
2023-10-20 19:57:18 +0500

    Task0V19

--- Tyuiu.IvanchikovDA.Sprint2.Task0.V19/Program.cs
+++ Tyuiu.IvanchikovDA.Sprint2.Task0.V19/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tyuiu.IvanchikovDA.Sprint2.Task0.V19.Lib;
+
 
 namespace Tyuiu.IvanchikovDA.Sprint2.Task0.V19
 {
@@ -10,6 +12,30 @@ namespace Tyuiu.IvanchikovDA.Sprint2.Task0.V19
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+            int x = 15;
+            int y = 16;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("x = " + y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
