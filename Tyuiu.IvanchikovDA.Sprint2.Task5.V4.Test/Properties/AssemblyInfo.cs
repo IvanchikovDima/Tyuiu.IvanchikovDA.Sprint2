7f15855ed516edc7d96d017caaef3c1d90357740
Tyuiu.IvanchikovDA@outlook.com <ivanchikov2022@internet.ru>
2023-10-21 03:05:02 +0500

    Task5V4

---
+++ Tyuiu.IvanchikovDA.Sprint2.Task5.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvanchikovDA.Sprint2.Task5.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvanchikovDA.Sprint2.Task5.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a5f8b1e-a600-4794-bed8-6b9e4ca3d904")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.IvanchikovDA.Sprint2.Task5.V4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IvanchikovDA.Sprint2.Task5.V4.Lib;
+
+namespace Tyuiu.IvanchikovDA.Sprint2.Task5.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер месяца: ");
+            int numMouth = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((numMouth < 1) || (numMouth > 12))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = "Это месяц: " + ds.FindMonthName(numMouth);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+
+        }
+    }
+}
