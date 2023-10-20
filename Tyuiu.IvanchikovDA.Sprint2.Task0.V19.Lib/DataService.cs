8350d9bbbafe7ea59fc6a146b7aff8d99d1a792a
Tyuiu.IvanchikovDA@outlook.com <ivanchikov2022@internet.ru>
2023-10-20 19:30:55 +0500

    Add project files.

---
+++ Tyuiu.IvanchikovDA.Sprint2.Task0.V19.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming
+
+namespace Tyuiu.IvanchikovDA.Sprint2.Task0.V19.Lib
+{
+    public class DataService
+    {
+    }
+}
---
+++ Tyuiu.IvanchikovDA.Sprint2.Task0.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvanchikovDA.Sprint2.Task0.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvanchikovDA.Sprint2.Task0.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3895a6db-a9af-46b6-9d75-05b1cab52988")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
