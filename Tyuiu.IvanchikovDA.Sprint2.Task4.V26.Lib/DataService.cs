5e7cdb49df0d1d90657583a7179cdc99474be3b8
Tyuiu.IvanchikovDA@outlook.com <ivanchikov2022@internet.ru>
2023-10-21 02:09:10 +0500

    Task4V26

---
+++ Tyuiu.IvanchikovDA.Sprint2.Task4.V26.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.IvanchikovDA.Sprint2.Task4.V26.Lib
+{
+    public class DataService : ISprint2Task4V26
+    {
+        public double Calculate(double x, double y)
+        {
+            double z = x > y ? (y / x + 3) : ((y - 1) / (x + 2));
+            return Math.Round(z,3);
+        }
+    }
+}
---
+++ Tyuiu.IvanchikovDA.Sprint2.Task4.V26.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.IvanchikovDA.Sprint2.Task4.V26.Lib;
+
+namespace Tyuiu.IvanchikovDA.Sprint2.Task4.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 5;
+            double res = ds.Calculate(x, y);
+            double wait = 3.5;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 15;
+            double res = ds.Calculate(x, y);
+            double wait = 1.167;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
